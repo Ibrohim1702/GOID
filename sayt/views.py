27dfdaceafaaa095b172e69aec2c3e0a6ba4a0d1
Ibrohim1702 <88259165+Ibrohim1702@users.noreply.github.com>
2023-07-26 15:15:53 +0500

    Admin, Contact
    Admin, Contact

--- sayt/views.py
+++ sayt/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 
+from sayt.models import Contact
+
+
 # Create your views here.
 
 
 def index(requests):
-    ctx = {
+    ctx = {}
+    if requests.POST:
+        name = requests.POST.get('name')
+        message = requests.POST.get('message')
+        phone = requests.POST.get('phone')
+        email = requests.POST.get('email')
+        Contact.objects.create(
+            name=name, message=message, phone=phone, email=email
+        )
 
-    }
+        ctx = {
+            "contact": contact,
+        }
     return render(requests, "index.html", ctx)
 
 def about(requests):
